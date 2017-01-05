d253d53ca062b1dbcb6af2a712fe7b98d05ba706
HeegyuKim <heekue83@gmail.com>
2017-01-05 23:11:23 +0900

    Yaho

--- application.py
+++ application.py
@@ -1,20 +1,24 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, g, render_template, request, redirect, url_for
 from datetime import datetime
-
+import sqlite3
 
 app = Flask(__name__)
 
+def get_db():
+    db = getattr(g, '_database', None)
 
-comments = [
-    {
-        'writer': '김희규',
-        'content': '많이 남은 것 같지? 많이 남았어'
-    },
-    {
-        'writer': '장준하',
-        'content': '♚♚히어로즈 오브 더 스☆톰♚♚가입시$$전원 카드팩☜☜뒷면100%증정※ ♜월드오브 워크래프트♜펫 무료증정￥ 특정조건 §§디아블로3§§★공허의유산★초상화♜오버워치♜겐지스킨￥획득기회@@@ 즉시이동http://kr.battle.net/heroes/ko/'
-    }
-]
+    if db is None:
+        db = g._database = sqlite3.connect("entry.db")
+    return db
+
+@app.teardown_appcontext
+def close_connection(expn):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+
 
 
 @app.route('/')
@@ -22,6 +26,8 @@ def index():
     countdown = datetime(2017, 11, 16) - datetime.now()
     dday = countdown.days
 
+    db = get_db()
+    comments = db.execute("SELECT * FROM entries LIMIT 10").fetchall()
     return render_template('index.html', countdown=dday, comments=comments)
 
 
@@ -32,13 +38,13 @@ def new_comment():
 
 @app.route('/new_comment', methods=['POST'])
 def post_comment():
-    print("작성자: %s" % request.form['writer'])
-    print("내용: %s" % request.form['content'])
 
-    comments.append({
-        'writer': request.form['writer'],
-        'content': request.form['content']
-    })
+    db = get_db()
+    db.execute(
+        "INSERT INTO entries (writer, content) VALUES (?, ?)",
+        (request.form['writer'], request.form['content'])
+        )
+    db.commit()
 
     return redirect(url_for('index'))
 
