2a535b9da75af58a701ee0a1d9109bec38545f6f
jangjunha <jangjunha113@gmail.com>
2017-01-09 20:37:47 +0900

    Apply SQLAlchemy

--- application.py
+++ application.py
@@ -1,36 +1,36 @@
 from flask import Flask, g, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import sqlite3
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    writer = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+    def __init__(self, writer, content):
+        self.writer = writer
+        self.content = content
+
+    def __repr__(self):
+        return '<Message %r>' % self.id
 
 
 def get_countdown():
     return datetime(2017, 11, 16) - datetime.now()
 
 
-def get_db():
-    db = getattr(g, '_database', None)
-
-    if db is None:
-        db = g._database = sqlite3.connect("satcounter.db")
-    return db
-
-
-@app.teardown_appcontext
-def close_connection(expn):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-
 @app.route('/')
 def index():
     countdown = get_countdown()
 
-    db = get_db()
-    comments = db.execute("SELECT * FROM comments LIMIT 10").fetchall()
+    comments = Message.query.order_by('-id').all()
     return render_template('index.html', countdown=countdown, comments=comments)
 
 
@@ -42,12 +42,10 @@ def new_comment():
 
 @app.route('/new_comment', methods=['POST'])
 def post_comment():
-    db = get_db()
-    db.execute(
-        "INSERT INTO comments (writer, content) VALUES (?, ?)",
-        (request.form['writer'], request.form['content'])
-    )
-    db.commit()
+    message = Message(writer=request.form['writer'],
+                      content=request.form['content'])
+    db.session.add(message)
+    db.session.commit()
 
     return redirect(url_for('index'))
 
