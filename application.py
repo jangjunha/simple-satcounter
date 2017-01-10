ba50b437c459d4df43dbe16eb39a80cb0b81005f
jangjunha <jangjunha113@gmail.com>
2017-01-10 10:03:24 +0900

    Implement login

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
-from flask import Flask, g, render_template, request, redirect, url_for
+from flask import Flask, g, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'development-key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
@@ -37,8 +38,16 @@ def get_countdown():
 def index():
     countdown = get_countdown()
 
+    user = None
+    if 'user_id' in session:
+        user_id = session['user_id']
+        user = User.query.filter_by(id=user_id).one()
+
     comments = Message.query.order_by('-id').all()
-    return render_template('index.html', countdown=countdown, comments=comments)
+    return render_template('index.html',
+                           countdown=countdown,
+                           comments=comments,
+                           user=user)
 
 
 @app.route('/new_comment')
@@ -70,6 +79,29 @@ def signup():
     user.password = generate_password_hash(request.form['password'])
     db.session.add(user)
     db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user is None:
+            error = '아이디 또는 비밀번호가 잘못되었습니다.'
+        elif not check_password_hash(user.password, request.form['password']):
+            error = '아이디 또는 비밀번호가 잘못되었습니다.'
+        else:
+            session['user_id'] = user.id
+            return redirect(url_for('index'))
+
+    countdown = get_countdown()
+    return render_template('login.html', countdown=countdown, error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
     return redirect(url_for('index'))
 
 
